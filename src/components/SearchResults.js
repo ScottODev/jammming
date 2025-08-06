ee8699a9bfa055c90bb51016365961c608855ec0
ScottODev <scottosullivan@proton.me>
2025-08-06 17:29:03 +0100

    Add no results message to SearchResults component

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,7 +2,16 @@ import React from 'react';
 import TrackList from './TrackList';
 
 function SearchResults({ tracks, onAdd }) {
-  return (
+  if (tracks.length === 0) { // If no songs are found, display message below
+    return (
+      <div>
+        <h2>Search Results</h2>
+        <p>No songs found. Try a different search!</p>
+      </div>
+    );
+  }
+
+  return ( // If songs are found, display below.
     <div>
       <h2>Search Results</h2>
       <TrackList tracks={tracks} onAdd={onAdd} />
